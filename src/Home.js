0969553692cd732d3166e9086d71323c8900fe77
Grégoire Joncour <greg.joncour@gmail.com>
2018-05-29 02:11:43 +0200

    serach cors home + update package

--- src/Home.js
+++ src/Home.js
@@ -10,18 +10,28 @@ export default class Home extends React.Component {
     }
 
     getDataAutocomplete(value) {
-        axios.get(`https://transilien.mobi/getProchainTrainAutocomplete?value=${encodeURI(value)}`, { crossdomain: true })
+        /**
+         * https://robwu.nl/cors-anywhere.html
+         * https://github.com/Rob--W/cors-anywhere/
+         * https://medium.com/netscape/hacking-it-out-when-cors-wont-let-you-be-great-35f6206cc646
+         */
+        axios.get(`https://cors-anywhere.herokuapp.com/https://transilien.mobi/getProchainTrainAutocomplete?value=${encodeURI(value)}`)
         .then(response => {
             console.log(response.data)
            // this.setState({trains: response.data.trains, station: response.data.station, isLoading: false})
         })
         .catch(error => {
-            console.log("error")
+            //console.log("error")
         });
     }
 
+    componentDidMount() {
+        /*var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://cors-escape.herokuapp.com/https://transilien.mobi/getProchainTrainAutocomplete?value=cloud");*/
+        document.body.style.backgroundColor = "#252438";
+    }
+
     handleChange (e) {
-        console.log(e.target.value)
         this.getDataAutocomplete(e.target.value)
         this.setState({valueSearch: e.target.value});
     }
