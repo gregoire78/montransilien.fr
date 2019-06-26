9858aadeb96f794021e7810608efe9040209cbeb
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2019-06-26 15:53:00 +0200

    nouveau format

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Monitor from './VianavigoMonitorSation';
-import Home from './Home';
+import Monitor from './StifMonitorStation';
+import HomeMap from './HomeMap';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={HomeMap}/>
           {/*<Route exact path="/:uic(\d{8})" component={Monitor}/>
           <Route exact path="/:uic(\d{7})" component={Monitor}/>*/}
           <Route exact path="/:uic" component={Monitor}/>
